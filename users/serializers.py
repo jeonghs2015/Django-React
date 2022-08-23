8ed39f116459bac1aa1d3f022bab4bb6bb4aaf30
hsjung2015 <hsjung201558@gmail.com>
2022-08-23 17:46:35 +0900

    serializers LoginSerializer 클래스 생성

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User  # User 모델
 from django.contrib.auth.password_validation import validate_password  #  Django의 기본 패스워드 검증 도구
+from django.contrib.auth import authenticate
+# Django의 기본 authenticate 함수, 우리가 설정한 DefaultAuthBackend인 TokenAuth 방식으로 유저를 인증해줌.
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token  #Token 모델
@@ -28,7 +30,7 @@ class RegisterSerializer(serializers.ModelSerializer):  # 회원가입 시리얼
             raise serializers.ValidationError(
                 {"password": "Password fields didn't match."}
             )
-            return data
+        return data
 
     def create(self, validated_data):
         # CREATE요청에 대해 create 메소드를 오버라이딩, 유저를 생성하고 토큰을 생성하게 함.
@@ -40,3 +42,17 @@ class RegisterSerializer(serializers.ModelSerializer):  # 회원가입 시리얼
         user.save()
         token = Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    # write_only 옵션을 통해 클라이언트->서버 방향의 역직렬화는 가능, 서버->클라이언트 방향의 직렬화는 불가능
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)  # 토큰에서 유저 찾아 응답
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials."}
+        )
