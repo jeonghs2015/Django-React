94356106d37513e9e2c703b3a28a838b2831ad03
hsjung2015 <hsjung201558@gmail.com>
2022-08-25 14:08:02 +0900

    view 페이지 수정

--- myboard/settings.py
+++ myboard/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMESORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        ],
+        'DEFAULT_FILTER_BACKENDS': [
+            'django_filters.rest_framework.DjangoFilterBackend',
         ],
 }
 
--- posts/views.py
+++ posts/views.py
@@ -4,11 +4,16 @@ from users.models import Profile
 from .models import Post
 from .permissions import CustomReadOnly
 from .serializers import PostSerializer, PostCreateSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+# view마다 필터 설정할 때 사용(settings.py에 이미 등록해서 상관없음)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [CustomReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['author', 'likes']
+
 
     def get_serializer_class(self):
         if self.action == 'list' or 'retrieve':
